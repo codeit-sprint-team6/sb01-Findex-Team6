f0df6b97bb196e30701e5aef66d74e990e7278a8
Lee Ju Nyung <139120379+JunyungLee@users.noreply.github.com>
2025-03-24 08:20:09 +0900

    Update ChartDataPoint.java

--- src/main/java/com/sprint/findex_team6/dto/dashboard/ChartDataPoint.java
+++ src/main/java/com/sprint/findex_team6/dto/dashboard/ChartDataPoint.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 @Schema(description = "차트 데이터 포인트 DTO")
 public record ChartDataPoint(
     @Schema(description = "날짜", example = "2025-03-18")
-    @JsonFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yy-MM")
     LocalDate baseDate,
 
     @Schema(description = "값", example = "2000.23")
